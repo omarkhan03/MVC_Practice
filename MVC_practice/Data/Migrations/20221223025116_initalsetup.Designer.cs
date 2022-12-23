1c82f325c2e361c1969ede2c861472e5a1ad20dc
Omar Khan <106503860+omarkhan03@users.noreply.github.com>
2022-12-22 19:58:11 -0700

    added jokes database

--- MVC_practice/Data/Migrations/20221223025116_initalsetup.Designer.cs
+++ MVC_practice/Data/Migrations/20221223025116_initalsetup.Designer.cs
@@ -4,6 +4,7 @@ using MVC_practice.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MVC_practice.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221223025116_initalsetup")]
+    partial class initalsetup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
